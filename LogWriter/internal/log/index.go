8d7e2111c5cfa0c8a91dda58de15f993f5fb92ff
rohit.ms <rohitmaheswaran27@gmail.com>
2025-01-05 22:52:03 +0530

    index operations

---
+++ LogWriter/internal/log/index.go
@@ -0,0 +1,54 @@
+package log
+
+import (
+	"os"
+
+	"github.com/tysonmote/gommap"
+)
+
+var offsetWidth uint64 = 4
+var positionWidth uint64 = 4
+var entireWidth uint64 = offsetWidth + positionWidth
+
+type index struct {
+	file *os.File
+	mmap gommap.MMap
+	size uint64
+}
+
+func newIndex(f *os.File, c Config) (*index, error) {
+	idx := &index{
+		file: f,
+	}
+	fi, err := os.Stat(f.Name())
+	if err != nil {
+		return nil, err
+	}
+	idx.size = uint64(fi.Size())
+	if err = os.Truncate(
+		f.Name(), int64(c.Segment.MaxIndexBytes),
+	); err != nil {
+		return nil, err
+	}
+	if idx.mmap, err = gommap.Map(
+		idx.file.Fd(),
+		gommap.PROT_READ|gommap.PROT_WRITE,
+		gommap.MAP_SHARED,
+	); err != nil {
+		return nil, err
+	}
+	return idx, nil
+}
+
+func (i *index) Close() error {
+	if err := i.mmap.Sync(gommap.MS_SYNC); err != nil {
+		return err
+	}
+	if err := i.file.Sync(); err != nil {
+		return err
+	}
+	if err := i.file.Truncate(int64(i.size)); err != nil {
+		return err
+	}
+	return nil
+}
