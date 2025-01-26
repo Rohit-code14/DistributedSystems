67dfb919034ed3a3fdc5a9e48cd00a64ee7ec2c0
rohit.ms <rohitmaheswaran27@gmail.com>
2025-01-26 20:29:13 +0530

    Log changes added

--- LogWriter/internal/log/log.go
+++ LogWriter/internal/log/log.go
@@ -1,12 +1,15 @@
 package log
 
 import (
+	"fmt"
 	"os"
 	"path"
 	"sort"
 	"strconv"
 	"strings"
 	"sync"
+
+	"github.com/Rohit-code14/LogWriter/internal/api"
 )
 
 type Log struct {
@@ -26,7 +29,7 @@ func NewLog(dir string, config Config) (*Log, error) {
 		Dir:    dir,
 		Config: config,
 	}
-	return l, nil
+	return l, l.setup()
 }
 
 func (l *Log) setup() error {
@@ -46,8 +49,96 @@ func (l *Log) setup() error {
 	})
 
 	for i := 0; i < len(baseOffsets); i++ {
-
+		if err := l.newSegment(baseOffsets[i]); err != nil {
+			return err
+		}
+		i++
 	}
 
+	if l.segments == nil {
+		if err := l.newSegment(l.Config.Segment.InitialOffset); err != nil {
+			return err
+		}
+	}
 	return nil
+}
+
+func (l *Log) newSegment(offset uint64) error {
+	s, err := newSegment(l.Dir, offset, l.Config)
+	if err != nil {
+		return err
+	}
+	l.segments = append(l.segments, s)
+	l.activeSegment = s
+	return nil
+}
+
+func (l *Log) Append(record *api.Record) (uint64, error) {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+	off, err := l.activeSegment.Write(record)
+	if err != nil {
+		return 0, err
+	}
+	if l.activeSegment.IsMaxed() {
+		err = l.newSegment(off + 1)
+	}
+	return off, err
+}
+
+func (l *Log) Read(offset uint64) (*api.Record, error) {
+	l.mu.RLock()
+	defer l.mu.RUnlock()
+	var s *segment
+	for _, segment := range l.segments {
+		if segment.baseOffset <= offset && offset < segment.nextOffset {
+			s = segment
+			break
+		}
+	}
+	if s == nil || s.nextOffset <= offset {
+		return nil, fmt.Errorf("Offset(%d) out of range", offset)
+	}
+	return s.Read(offset)
+}
+
+func (l *Log) Close() error {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+	for _, segment := range l.segments {
+		if err := segment.Close(); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (l *Log) Remove() error {
+	if err := l.Close(); err != nil {
+		return err
+	}
+	return os.RemoveAll(l.Dir)
+}
+
+func (l *Log) Reset() error {
+	if err := l.Remove(); err != nil {
+		return err
+	}
+	return l.setup()
+}
+
+func (l *Log) LowestOffset() uint64 {
+	l.mu.RLock()
+	defer l.mu.RUnlock()
+	return l.segments[0].baseOffset
+}
+
+func (l *Log) HighestOffset() uint64 {
+	l.mu.RLock()
+	defer l.mu.RUnlock()
+	offset := l.segments[len(l.segments)-1].nextOffset
+	if offset == 0 {
+		return 0
+	}
+	return offset - 1
 }
---
+++ LogWriter/internal/log/log_test.go
@@ -0,0 +1,69 @@
+package log
+
+import (
+	"os"
+	"testing"
+
+	"github.com/Rohit-code14/LogWriter/internal/api"
+	"github.com/stretchr/testify/require"
+)
+
+func TestLog(t *testing.T) {
+	for scenario, fn := range map[string]func(
+		t *testing.T, log *Log,
+	){
+		"append and read a record succeeds": testAppendRead,
+		"offset out of range error":         testOutOfRangeErr,
+		"init with existing segments":       testInitExisting,
+	} {
+		t.Run(scenario, func(t *testing.T) {
+			dir, err := os.MkdirTemp("", "segment_test")
+			require.NoError(t, err)
+			defer os.RemoveAll(dir)
+			c := Config{}
+			c.Segment.MaxStoreBytes = 32
+			log, err := NewLog(dir, c)
+			require.NoError(t, err)
+			fn(t, log)
+		})
+	}
+}
+
+func testAppendRead(t *testing.T, log *Log) {
+	append := &api.Record{
+		Value: []byte("hello world"),
+	}
+	off, err := log.Append(append)
+	require.NoError(t, err)
+	require.Equal(t, uint64(0), off)
+	read, err := log.Read(off)
+	require.NoError(t, err)
+	require.Equal(t, append.Value, read.Value)
+}
+
+func testOutOfRangeErr(t *testing.T, log *Log) {
+	read, err := log.Read(1)
+	require.Nil(t, read)
+	require.Error(t, err)
+}
+
+func testInitExisting(t *testing.T, o *Log) {
+	append := &api.Record{
+		Value: []byte("hello world"),
+	}
+	for i := 0; i < 3; i++ {
+		_, err := o.Append(append)
+		require.NoError(t, err)
+	}
+	require.NoError(t, o.Close())
+	off := o.LowestOffset()
+	require.Equal(t, uint64(0), off)
+	off = o.HighestOffset()
+	require.Equal(t, uint64(2), off)
+	n, err := NewLog(o.Dir, o.Config)
+	require.NoError(t, err)
+	off = n.LowestOffset()
+	require.Equal(t, uint64(0), off)
+	off = n.HighestOffset()
+	require.Equal(t, uint64(2), off)
+}
